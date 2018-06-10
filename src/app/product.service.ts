f575f072f63b8fa10ea12c0cd2f3f98f8c074baa
Matt Butalla <butallmj@gmail.com>
2018-06-10 13:15:10 -0500

    step 3.1 - picky picky

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http'
 import { Observable } from 'rxjs/Observable';
-import {} from 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ProductService {
@@ -10,7 +10,7 @@ export class ProductService {
 
   constructor(private _http:Http) { }
 
-  getAlbum(id: number): Observable<Response> {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => response.json());
   }
 
